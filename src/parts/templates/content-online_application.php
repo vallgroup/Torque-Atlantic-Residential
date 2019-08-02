c0843d20533f48e5ef8b3b878babf2f0693fbcb5
Al Notara <al@vallgroup.com>
2019-08-02 16:19:54 -0700

    Added page template and template parts for Online Applications

---
+++ src/parts/templates/content-online_application.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template part for displaying page content in online-application.php
+ *
+ * @package Torque
+ */
+
+get_template_part('/parts/forms/form-online-application');
+
+?>
---
+++ src/parts/templates/titles/title-online_application.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Online Application | Title Template
+ **/
+?>
+
+<div class="online-application-title-wrapper" >
+    <h1>
+        <?php the_title(); ?>
+    </h1>
+</div>
