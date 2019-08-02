09248d5da6fbb825f259fb8cc37c31c51f8a106e
Al Notara <al@vallgroup.com>
2019-08-02 16:22:03 -0700

    Added file upload JS for Careers Form

--- src/js/careersFormExtras.js
+++ src/js/careersFormExtras.js
@@ -5,6 +5,18 @@
             $('#careers-form input#tq-name').focus();
             $('#careers-form input#tq-job').val($(this).text());
         });
+
+        $('input[type="file"]#tq-resume').bind('change', function() {
+            var fullPath = $(this).val();
+            if (fullPath) {
+                var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+                var filename = fullPath.substring(startIndex);
+                if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+                    filename = filename.substring(1);
+                }
+                $('.file-input-text').html(filename).removeClass('placeholder');
+            }
+        });
     });
 })(jQuery);
   
