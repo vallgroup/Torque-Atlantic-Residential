fda5e9d30ed085ad6ca41b8a08d80bd52c6e29b1
Al Notara <al@vallgroup.com>
2019-08-15 14:22:04 -0700

    Added tagline to property listing pages

--- src/parts/templates/titles/title-listings.php
+++ src/parts/templates/titles/title-listings.php
@@ -1,11 +1,15 @@
 <?php
 
+$tagline = get_field('tagline');
 $heading = get_field('heading');
 $intro = get_field('intro');
 
 ?>
 
 <div class="listings-title-wrapper" >
+  <?php if ($tagline) { ?>
+    <h5><?php echo $tagline; ?></h5>
+  <?php } ?>
   <?php if ($heading) { ?>
     <h1><?php echo $heading; ?></h1>
   <?php } ?>
--- src/parts/templates/titles/title-torque_listing.php
+++ src/parts/templates/titles/title-torque_listing.php
@@ -6,13 +6,21 @@ $title = get_the_title();
 
 ?>
 
+<div class="listing-title-wrapper hide-on-desktop" >
+  <?php if ($logo_src) { ?>
+    <img class="property-logo" src="<?php echo $logo_src; ?>"/>
+  <? } else { ?>
+    <h2><?php the_title(); ?></h2>
+  <?php } ?>
+</div>
+
 <div class="page-hero type-image content-position-top align-center height-half">
   <div class="hero-image-size">
     <div class="hero-image" style="background-image: url(<?php echo $image_src; ?>);" ></div>
   </div>
 </div>
 
-<div class="listing-title-wrapper" >
+<div class="listing-title-wrapper hide-on-tablet" >
   <?php if ($logo_src) { ?>
     <img class="property-logo" src="<?php echo $logo_src; ?>"/>
   <? } else { ?>
