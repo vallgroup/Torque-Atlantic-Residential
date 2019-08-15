69162ab4ff6c1111d0d2479d3bfb2689ebb52ee8
Al Notara <al@vallgroup.com>
2019-08-15 14:13:01 -0700

    Added more login to style search form on page resize

--- src/js/listingArchive.js
+++ src/js/listingArchive.js
@@ -4,6 +4,9 @@
         const searchForm = $('form.searchform');
         const searchFormSubmitBtn = searchForm.find('button[type=submit]');
 
+        // Style search form on page load
+        styleSearchForm();
+
         searchFormSubmitBtn.click(function(e){
             if (searchForm.hasClass('closed')) {
                 // Don't submit the form
@@ -13,5 +16,18 @@
                 searchForm.removeClass('closed');
             }
         });
+
+        $(window).resize(function(){
+            // Style search form on window resize
+            styleSearchForm();
+        });
+
+        function styleSearchForm() {
+            if ( $(window).width() >= 1023 ) {
+                searchForm.addClass('closed');
+            } else {
+                searchForm.removeClass('closed');
+            }
+        }
     });
 })(jQuery);
