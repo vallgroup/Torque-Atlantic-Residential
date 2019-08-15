9125803872d816f62357104de1d6b24fe9adcd8b
Al Notara <al@vallgroup.com>
2019-08-15 14:18:30 -0700

    Loaded both light and dark logos, and show based on css class

--- src/parts/shared/header-templates/header-style-1.php
+++ src/parts/shared/header-templates/header-style-1.php
@@ -14,9 +14,14 @@
 // Style the header based on the 
 $header_light_dark = get_field('light_or_dark_header') === 'light' ? 'light' : 'dark';
 
-$logo_light_dark = get_field('light_or_dark_header') === 'light' ? 'white' : 'dark';
+// $logo_light_dark = get_field('light_or_dark_header') === 'light' ? 'white' : 'dark';
 
 $extra_classes = isset($tq_header_style_1_classes) ? $tq_header_style_1_classes : '';
+
+if ( isset($enforce_dark_header) && $enforce_dark_header || is_search() ) {
+  $header_light_dark = 'dark';
+  $logo_light_dark = 'dark';
+}
 
 ?>
 
@@ -27,7 +32,9 @@ $extra_classes = isset($tq_header_style_1_classes) ? $tq_header_style_1_classes 
   <div class="row torque-header-content-wrapper torque-navigation-toggle">
 
     <div class="col2 col2-tablet col2-desktop torque-header-logo-wrapper">
-      <?php get_template_part( 'parts/shared/logo', $logo_light_dark); ?>
+      <?php // get_template_part( 'parts/shared/logo', $logo_light_dark); ?>
+      <?php get_template_part( 'parts/shared/logo', 'dark'); ?>
+      <?php get_template_part( 'parts/shared/logo', 'white'); ?>
     </div>
 
     <div class="col2 col2-tablet col2-desktop torque-header-burger-menu-wrapper">
@@ -37,11 +44,11 @@ $extra_classes = isset($tq_header_style_1_classes) ? $tq_header_style_1_classes 
   </div>
 
   <div class="col1 torque-navigation-toggle torque-header-menu-items-mobile">
-    <svg class="diagonal-top" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+    <svg class="diagonal-top" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none" shape-rendering="geometricPrecision">
       <polygon points="0,100 50,0 100,0 100,100"/>
     </svg>
     <?php get_template_part( 'parts/shared/header-parts/menu-items/menu-items', 'stacked'); ?>
-    <svg class="diagonal-bottom" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+    <svg class="diagonal-bottom" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none" shape-rendering="geometricPrecision">
       <polygon points="0,0 100,0 100,100"/>
     </svg>
   </div>
