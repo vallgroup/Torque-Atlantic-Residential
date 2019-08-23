e243b559b7fb73bd754d375459cbb1284bf1ab8d
Al Notara <al@vallgroup.com>
2019-08-23 14:28:43 -0700

    Further minor tweaks to styling

--- src/js/teamImageResize.js
+++ src/js/teamImageResize.js
@@ -4,7 +4,7 @@
     const single_listing_team_headshot = $(".broker .broker-image-container");
     const team_grid_headshot = $(".staff-member .image-wrapper");
     const single_team_headshot = $(".staff-image-container .featured-image");
-    const height_to_width_ratio = 1.31;
+    const height_to_width_ratio = 1.28;
 
     // On load ensure image ratio is correct
     resetImageRatio(single_listing_team_headshot, height_to_width_ratio);
