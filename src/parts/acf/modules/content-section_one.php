e15021f1e67b4ec8f839415b6762d943b92d845d
Al Notara <al@vallgroup.com>
2019-09-17 11:39:23 -0700

    Re-factored code to suit Lena's designs (text on top, on mobile).

--- src/parts/acf/modules/content-section_one.php
+++ src/parts/acf/modules/content-section_one.php
@@ -1,24 +1,47 @@
-<div
-  class="content-section align-<?php echo $align; ?> <?php echo $top_bottom_padding; ?>">
+<div class="content-section align-<?php echo $align; ?> <?php echo $top_bottom_padding; ?>">
 
-  <div class="content-section-image-size <?php echo $reduced_height; ?>">
-    <div class="content-section-image background-image-<?php echo $image_background_size ?>" style="background-image: url(<?php echo $image; ?>);" ></div>
-  </div>
-
-  <div class="content-wrapper" >
+<?php /* Hide on tablet and desktop */ ?>
+  <div class="content-wrapper hide-on-tablet hide-on-desktop">
     <h3><?php echo $heading; ?></h3>
 
     <?php if ($content) { ?>
       <div class="content"><?php echo $content; ?></div>
     <?php } ?>
-
     <?php if ($cta) { ?>
-      <div class="cta-wrapper" >
+      <div class="cta-wrapper">
         <a href="<?php echo $cta['url']; ?>" target="<?php echo $cta['target']; ?>">
           <button class=""><?php echo $cta['title']; ?></button>
         </a>
       </div>
     <?php } ?>
   </div>
+<?php /* --------- */ ?>
+
+  <div class="content-section-image-size <?php echo $reduced_height; ?>">
+    <div class="content-section-image background-image-<?php echo $image_background_size ?>" style="background-image: url(<?php echo $image_url; ?>);"></div>
+    <?php if ($image_caption != '') { ?>
+      <div class="image-caption hide-on-tablet hide-on-desktop"><?php echo $image_caption; ?></div>
+    <?php } ?>
+  </div>
+
+<?php /* Hide on mobile */ ?>
+  <div class="content-wrapper hide-on-mobile">
+    <h3><?php echo $heading; ?></h3>
+
+    <?php if ($content) { ?>
+      <div class="content"><?php echo $content; ?></div>
+    <?php } ?>
+    <?php if ($cta) { ?>
+      <div class="cta-wrapper">
+        <a href="<?php echo $cta['url']; ?>" target="<?php echo $cta['target']; ?>">
+          <button class=""><?php echo $cta['title']; ?></button>
+        </a>
+      </div>
+    <?php } ?>
+    <?php if ($image_caption != '') { ?>
+      <div class="image-caption"><?php echo $image_caption; ?></div>
+    <?php } ?>
+  </div>
+<?php /* --------- */ ?>
 
 </div>
--- src/parts/acf/modules/content-section_three.php
+++ src/parts/acf/modules/content-section_three.php
@@ -1,29 +1,62 @@
 <div class="content-section-three align-<?php echo $align; ?> background-<?php echo $background_color; ?>">
 
   <div class="content-container">
-    <div class="content-section-image-size">
-      <div class="content-section-image" style="background-image: url(<?php echo $image; ?>);" ></div>
-    </div>
 
-    <div class="content-wrapper" >
+  <?php /* Hide on tablet and desktop */ ?>
+    <div class="content-wrapper hide-on-tablet hide-on-desktop">
       <div class="bg-quotation-mark-top"></div>
       <div class="bg-quotation-mark-bottom"></div>
       <h3><?php echo $quote; ?></h3>
     </div>
-  </div>
-  
-  <?php if ($content || $cta) { ?>
-  <div class="cta-wrapper" >
-  <?php if ($content) { ?>
-    <div class="content"><?php echo $content; ?></div>
-  <?php } ?>
+    <?php if ($content || $cta) { ?>
+      <div class="cta-wrapper hide-on-tablet hide-on-desktop">
+        <?php if ($content) { ?>
+          <div class="content"><?php echo $content; ?></div>
+        <?php } ?>
 
-  <?php if ($cta) { ?>
-    <a href="<?php echo $cta['url']; ?>" target="<?php echo $cta['target']; ?>">
-      <button class="<?php echo $button_light; ?>"><?php echo $cta['title']; ?></button>
-    </a>
-  <?php } ?>
+        <?php if ($cta) { ?>
+          <a href="<?php echo $cta['url']; ?>" target="<?php echo $cta['target']; ?>">
+            <button class="<?php echo $button_light; ?>"><?php echo $cta['title']; ?></button>
+          </a>
+        <?php } ?>
+      </div>
+    <?php } ?>
+  <?php /* --------- */ ?>
+
+    <div class="content-section-image-size">
+      <div class="content-section-image" style="background-image: url(<?php echo $image_url; ?>);"></div>
+      <?php if ($image_caption != '') { ?>
+        <div class="image-caption hide-on-tablet hide-on-desktop"><?php echo $image_caption; ?></div>
+      <?php } ?>
+    </div>
+
+  <?php /* Hide on mobile */ ?>
+    <div class="content-wrapper hide-on-mobile">
+      <div class="bg-quotation-mark-top"></div>
+      <div class="bg-quotation-mark-bottom"></div>
+      <h3><?php echo $quote; ?></h3>
+      <?php if ($image_caption != '') { ?>
+        <div class="image-caption"><?php echo $image_caption; ?></div>
+      <?php } ?>
+    </div>
+  <?php /* --------- */ ?>
+
   </div>
-<?php } ?>
+
+  <?php if ($content || $cta) { ?>
+  <?php /* Hide on mobile */ ?>
+    <div class="cta-wrapper hide-on-mobile">
+      <?php if ($content) { ?>
+        <div class="content"><?php echo $content; ?></div>
+      <?php } ?>
+
+      <?php if ($cta) { ?>
+        <a href="<?php echo $cta['url']; ?>" target="<?php echo $cta['target']; ?>">
+          <button class="<?php echo $button_light; ?>"><?php echo $cta['title']; ?></button>
+        </a>
+      <?php } ?>
+    </div>
+  <?php /* --------- */ ?>
+  <?php } ?>
 
 </div>
