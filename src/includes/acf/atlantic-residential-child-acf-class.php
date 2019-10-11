3e0d5cdcafca984acabe73a5d78011a735d0649f
Al Notara <al@vallgroup.com>
2019-10-11 11:00:20 -0700

    Uncommented filter to hide ACF fields in backend

--- src/includes/acf/atlantic-residential-child-acf-class.php
+++ src/includes/acf/atlantic-residential-child-acf-class.php
@@ -11,7 +11,7 @@ class Atlantic_Residential_ACF
     add_action('acf/init', array($this, 'acf_init'));
 
     // hide acf in admin - client doesnt need to see this
-    // add_filter('acf/settings/show_admin', '__return_false');
+    add_filter('acf/settings/show_admin', '__return_false');
 
     // add acf fields to wp search
     if (class_exists('Torque_ACF_Search')) {
